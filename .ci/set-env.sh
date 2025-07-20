64217502ac60472f4620525ef250542103968c05
0x1def <0x1def@flakybit.net>
2025-07-20 21:46:14 +0300

    Version bump

--- .ci/set-env.sh
+++ .ci/set-env.sh
@@ -14,7 +14,7 @@ app_component='server'
 printf 'APP_COMPONENT=%s\n' "$app_component" >> "$CI_ENV_FILE"
 
 printf 'APP_VERSION=%s\n' "$(getAppVersion)" >> "$CI_ENV_FILE"
-printf 'NEXTCLOUD_VERSION=%s\n' '30.0.10' >> "$CI_ENV_FILE"
+printf 'NEXTCLOUD_VERSION=%s\n' '30.0.13' >> "$CI_ENV_FILE"
 
 printf 'HARBOR_REGISTRY=%s\n' 'harbor.flakybit.net' >> "$CI_ENV_FILE"
 printf 'EXTERNAL_REGISTRY_NAMESPACE=%s\n' 'flakybitnet' >> "$CI_ENV_FILE"
